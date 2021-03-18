6f0241f614061fd3a9732408b7c16ab1c18b635d
Poreniyi <poreniyi@gmail.com>
2021-03-18 19:21:14 -0400

    set up intial configurations

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-let express= require('express');
-let app= express();
+let express = require('express');
+let app = express();
+let path = require('path');
 
-app
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+
+app.use((req, res, next) => {
+    res.status(404).render('home')
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Listening for requests onn port${app.get('port')}`)
+})
