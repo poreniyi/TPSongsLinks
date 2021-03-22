fb11e3b30eede81d245c853876de9acb8297d398
Poreniyi <poreniyi@gmail.com>
2021-03-22 15:04:30 -0400

    added checking of mp3file values vs txtfile values

--- musicTest.js
+++ musicTest.js
@@ -1,5 +1,7 @@
 let path=require('path');
 let tagger=require('music-metadata');
+let readTexts=require('./readSourcesTxt').readFile;
+let fs=require('fs').promises;
 
 let readMP3=async()=>{
     let testFile=path.join(__dirname,"../musicfiles/Ancient.mp3");
@@ -10,4 +12,37 @@ let readMP3=async()=>{
     let songTitle=metadata.common.title;
     console.log(metadata);
 }
-readMP3()
+//readMP3()
+
+let readAllMP3=async()=>{
+    let musicFileList=await fs.readdir(path.join(__dirname,'../musicfiles'));
+    let mp3SongList=[];
+    for(let i=0;i<musicFileList.length;i++){
+        let metadata=await tagger.parseFile(path.join(__dirname,`../musicfiles/${musicFileList[i]}`));
+       let data= metadata.common.title.trim().toLowerCase();
+       data=data.replace(/\s+/g, '')
+        mp3SongList.push(data);
+    }
+    console.log(mp3SongList.length)
+    let songList=await readTexts();
+    let counter=0;
+    songList.forEach(element=>{
+        if(!(mp3SongList.indexOf(element))){
+            console.log(`${element} is not in the mp3list`)
+            counter++
+        }
+    })
+    mp3SongList.sort()
+    songList.sort()
+    for(let i=0;i<songList.length;i++){
+        let truthy=songList[i]==mp3SongList[i];
+        if(!truthy){
+            console.log(`txt file is ${songList[i]}|${mp3SongList[i]}`)
+            break;
+        }
+    }
+    console.log(`There are ${counter} discrepencies`);  
+    console.log(songList[29]==mp3SongList[29]);  
+}
+
+readAllMP3()
--- readSourcesTxt.js
+++ readSourcesTxt.js
@@ -10,20 +10,33 @@ let readFile = async () => {
     for (let i = 4; i < lines.length; i++) {
         let element = lines[i].replace(/(\r\n|\n|\r|\t)/gm, "");
         if (element.startsWith('|')) {
-            Sources[sourceCounter].source+= element;
+            Sources[sourceCounter].source += element;
             continue;
         }
         if (element.match(sourceRegex)) {
-            Sources.push({ source: element,songs:[]});
+            Sources.push({ source: element, songs: [] });
             sourceCounter++;
         }
         if (element.match(songRegex)) {
-            Sources[sourceCounter].songs.push(element);
+            let value = element.replace(songRegex, "").toLowerCase();
+            value=value.replace(/\s+/g, '')
+            Sources[sourceCounter].songs.push(value.trim());
         }
     }
-    console.log(Sources);
-    let songCounter=(accumulator,currentValue)=>accumulator+currentValue.songs.length
-    let songsCount=Sources.reduce(songCounter,0)
-    console.log(songsCount);
+   // console.log(Sources);
+    let songCounter = (accumulator, currentValue) => accumulator + currentValue.songs.length
+    let songsCount = Sources.reduce(songCounter, 0)
+    //console.log(songsCount);
+    let uniqueSongs=[];
+    Sources.forEach(row=>{
+        row.songs.forEach(element=>{
+            if(!uniqueSongs.includes(element))uniqueSongs.push(element)
+        })
+    })
+   // console.log(uniqueSongs.length)
+    return uniqueSongs;
 }
-readFile();
+//readFile();
+module.exports={
+    readFile:readFile,
+}
