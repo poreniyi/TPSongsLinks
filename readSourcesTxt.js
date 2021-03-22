1c02a57bc5e6c20eec96059b86b2c4028d193928
Poreniyi <poreniyi@gmail.com>
2021-03-22 12:18:02 -0400

    combined arrays into 1 array of objects

--- readSourcesTxt.js
+++ readSourcesTxt.js
@@ -2,34 +2,25 @@ let fs = require('fs').promises;
 let path = require('path');
 let readFile = async () => {
     let Sources = [];
-    let Songs = [];
     let data = await fs.readFile(path.join(__dirname, "txtFiles/Touhou Music Sources.txt"), 'utf-8');
     let lines = data.split('\n');
     let sourceRegex = /^[^\d+.]/;
     let songRegex = /^\d+\)/
-    let currentSource = {};
+    let sourceCounter = -1;
     for (let i = 4; i < lines.length; i++) {
         let element = lines[i].replace(/(\r\n|\n|\r|\t)/gm, "");
-        if(element.startsWith('|')){
-           // console.log(element);
-            lines[i-1]+=element;
-            lines.splice(i,1);
-            i--;
+        if (element.startsWith('|')) {
+            Sources[sourceCounter].source+= element;
             continue;
         }
         if (element.match(sourceRegex)) {
-            currentSource = element;
-            Sources.push(element);
+            Sources.push({ source: element,songs:[]});
+            sourceCounter++;
         }
         if (element.match(songRegex)) {
-            Songs.push({
-                song: element,
-                source: currentSource,
-            });
+            Sources[sourceCounter].songs.push(element);
         }
     }
-
-    console.log(Sources.length);
-   // console.log(Songs);
+    console.log(Sources);
 }
 readFile();
