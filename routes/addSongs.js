53b167fb222c7f2d81f2539b8d0b6e9bd84a1553
Poreniyi <poreniyi@gmail.com>
2021-03-23 09:33:51 -0400

    added passing data to view

--- routes/addSongs.js
+++ routes/addSongs.js
@@ -1,7 +1,12 @@
-let db=require('./DB/access');
-let router=require('express').Router();
+let query = require('../DB/access').query;
+let router = require('express').Router();
 
 
-router.get('/add',(req,res)=>{
-    
+router.get('/getSongs',async (req, res) => {
+    let data = await query(`SELECT * FROM SONGS`);
+    data=data.rows;
+    console.log(data);
+    res.render('viewSongs',{data})
 })
+
+module.exports=router;
