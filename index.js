87331d53f7bad7f264cae0b88994e8cea9f16c23
Poreniyi <poreniyi@gmail.com>
2021-03-24 20:17:34 -0400

    created public folder with stylesheets

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/', async(req, res) => {
     res.render('home');
 })
