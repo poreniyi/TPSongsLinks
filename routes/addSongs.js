980782c5c6cf59843ad167051d7d30d5f16a3f00
Poreniyi <poreniyi@gmail.com>
2021-04-09 17:26:48 -0400

    added images to table

--- routes/addSongs.js
+++ routes/addSongs.js
@@ -1,14 +1,20 @@
 let query = require('../DB/access').query;
 let router = require('express').Router();
 let musicParser = require('music-metadata');
+let fs = require('fs').promises;
+let path = require('path');
 
-
-router.get('/getSongs',async (req, res) => {
+router.get('/getSongs', async (req, res) => {
     let data = await query(`SELECT * FROM SONGS ORDER BY id ASC`);
-    let musicFileList = await fs.readdir(path.join(__dirname, '../../../musicfiles'));
-    data=data.rows;
-    console.log(data);
-    res.render('viewSongs',{data})
+    let firstSong;
+    [firstSong] = await fs.readdir(path.join(__dirname, '../../musicfiles'));
+    let metadata = await musicParser.parseFile(path.join(__dirname, `../../musicfiles/${firstSong}`));
+    console.log(metadata.common.picture[0])
+    data = data.rows;
+    let img = { format: metadata.common.picture[0].format, base: metadata.common.picture[0].data.toString('base64') }
+    //console.log(img.base)
+    // console.log(data);
+    res.render('viewSongs', { data, img })
 })
 
-module.exports=router;
+module.exports = router;
