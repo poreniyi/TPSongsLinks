17116df7c5322388678975d22d0358753cbd8181
Poreniyi <poreniyi@gmail.com>
2021-04-09 21:16:27 -0400

    added converting titles to hash

--- musicTest.js
+++ musicTest.js
@@ -2,8 +2,7 @@ let path = require('path');
 let tagger = require('music-metadata');
 let readTexts = require('./readSourcesTxt').readFile;
 let fs = require('fs').promises;
-let db = require('./DB/access');
-
+const {createHash}= require('crypto')
 
 let readMP3 = async () => {
     let testFile = path.join(__dirname, "../musicfiles/Night that can be Danced.mp3");
@@ -13,35 +12,47 @@ let readMP3 = async () => {
     let Circles = metadata.common.albumartist;
     let songTitle = metadata.common.title;
     //console.log(metadata.native['ID3v2.3']);
-    console.log(metadata.format.duration);
+    //console.log(metadata.format.duration);
+    hash.update(songTitle);
+    let hashValue= hash.digest('hex')
+    console.log(hashValue);
     //console.log(metadata.native['ID3v2.3']);
     //console.log(metadata.common)//comments not broken up
 }
-//readMP3()
 
 let readAllMP3 = async () => {
     let musicFileList = await fs.readdir(path.join(__dirname, '../musicfiles'));
     let mp3SongList = [];
+    let testValue;
     for (let i = 0; i < musicFileList.length; i++) {
         let metadata = await tagger.parseFile(path.join(__dirname, `../musicfiles/${musicFileList[i]}`));
-        let data = metadata.common.title.trim().toLowerCase();
-        mp3SongList.push(data);
-    }
-    console.log(mp3SongList.length)
-    let songList = await readTexts();
-    let counter = 0;
-    mp3SongList.sort()
-    songList.sort()
-    for (let i = 0; i < songList.length; i++) {
-        let truthy = songList[i] == mp3SongList[i];
-        if (!truthy) {
-            console.log(`txt file is ${songList[i]}|${mp3SongList[i]}`)
-            break;
+        let data = metadata.common.title.replace(/\s+/g, '').toLowerCase();
+        if (i==209){
+            testValue=metadata.common.title.toLowerCase();
         }
+       // console.log(data)
+        const hash=createHash('sha256')
+        hash.update(data)
+        let hashValue=hash.digest('hex');
+        mp3SongList.push(hashValue);
     }
-    console.log(`There are ${counter} discrepencies`);
-    console.log(songList[29] == mp3SongList[29]);
+
+    let textFileSongList = await readTexts();
+    let badValues=[];
+    textFileSongList.forEach(element=>{
+        let data=element.replace(/\s+/g, '')
+        const hash=createHash('sha256')
+        hash.update(data)
+        let hashValue=hash.digest('hex');
+        if(!mp3SongList.includes(hashValue)) {
+            badValues.push(element)
+            console.log(element)
+        }
+    })
+    console.log(`Textfile${badValues[0]} is equal to ${testValue}\n${badValues[0]==testValue}`)
+    // console.log(badValues.length)
+    //console.log(textFileSongList)
+
 }
-//db.query('CREATE TABLE IF NOT EXISTS VOCALISTS')
-readMP3()
-//readAllMP3()
+//readMP3()
+readAllMP3()
