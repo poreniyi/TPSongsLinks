a3ec24ca5a2b4b247b668098f3808cd5bd97afd8
Poreniyi <poreniyi@gmail.com>
2021-04-13 12:55:23 -0400

    added songs circle_songs and circles_arrangers

--- DB/setQueries/createTabels.sql
+++ DB/setQueries/createTabels.sql
@@ -1,18 +1,44 @@
+---One to many
+CREATE TABLE IF NOT EXISTS SONGS (
+	id SERIAL PRIMARY KEY,
+    Name TEXT UNIQUE,
+	Hash TEXT UNIQUE,
+    Length float(5),
+	BPM float(5),
+    Year SMALLINT,
+	album_id INT,
+	CONSTRAINT fk_album FOREIGN KEY  (album_id) REFERENCES albums(id)
+ );
+ ----Many to many
 CREATE TABLE IF NOT EXISTS song_vocalists (
 	song_id INT,
 	vocalist_id INT,
  	CONSTRAINT fk_song FOREIGN KEY (song_id) REFERENCES songs(id),
 	CONSTRAINT fk_vocalist FOREIGN KEY  (vocalist_id) REFERENCES vocalists(id)
  );
-CREATE TABLE IF NOT EXISTS song_albums (
+CREATE TABLE IF NOT EXISTS song_arrangers (
 	song_id INT,
-	album_id INT,
+	arranger_id INT,
  	CONSTRAINT fk_song FOREIGN KEY (song_id) REFERENCES songs(id),
-	CONSTRAINT fk_vocalist FOREIGN KEY  (album_id) REFERENCES albums(id)
+	CONSTRAINT fk_arranger FOREIGN KEY  (arranger_id) REFERENCES arrangers(id)
  );
+
 CREATE TABLE IF NOT EXISTS circle_albums (
 	circle_id INT,
 	album_id INT,
- 	CONSTRAINT fk_song FOREIGN KEY (circle_id) REFERENCES circles(id),
-	CONSTRAINT fk_vocalist FOREIGN KEY  (album_id) REFERENCES albums(id)
+ 	CONSTRAINT fk_circle FOREIGN KEY (circle_id) REFERENCES circles(id),
+	CONSTRAINT fk_album FOREIGN KEY  (album_id) REFERENCES albums(id)
+ );
+ CREATE TABLE IF NOT EXISTS circle_arrangers (
+	circle_id INT,
+	arranger_id INT,
+ 	CONSTRAINT fk_circle FOREIGN KEY (circle_id) REFERENCES circles(id),
+	CONSTRAINT fk_vocalist FOREIGN KEY  (arranger_id) REFERENCES arrangers(id)
+ );
+ 
+  CREATE TABLE IF NOT EXISTS circle_songs (
+	circle_id INT,
+	song_id INT,
+ 	CONSTRAINT fk_circle FOREIGN KEY (circle_id) REFERENCES circles(id),
+	CONSTRAINT fk_song FOREIGN KEY  (song_id) REFERENCES songs(id)
  );
