d0d722cdcf16a06b14b013c0bf558fdea7d556f0
Poreniyi <poreniyi@gmail.com>
2021-04-13 17:44:26 -0400

    added begining of song insertion query

--- DB/setQueries/insertMetadata.js
+++ DB/setQueries/insertMetadata.js
@@ -2,6 +2,8 @@ let musicParser = require('music-metadata');
 let query = require('../access').query
 let fs = require('fs').promises
 let path = require('path');
+const { createHash } = require('crypto')
+let Jimp = require('jimp');
 
 let insertData = async () => {
     let currentAlbumId;
@@ -19,20 +21,34 @@ let insertData = async () => {
             Circles = Circles || ''
             let circleList = [];
             circleList = Circles.split('/');
-            // if (Circles.includes('/')) {
-            //     console.log(title);
-            // }
-            let { rows: [ album_id ] } = await query(`INSERT INTO ALBUMS(Name)
-               VALUES($1) ON CONFLICT (Name) DO NOTHING RETURNING id`, [album]) 
-            if(!album_id) ({ rows: [ album_id ] }= await query(`SELECT id FROM Albums Where Name = $1;`,[album]));
-             console.log(album_id)
+            const hash = createHash('sha256');
+            hash.update(metadata.common.title.replace(/\s+/g, '').toLowerCase());
+            let hashValue = hash.digest('hex');
+            let image = await Jimp.read(metadata.common.picture[0].data)
+            image.quality(60);
+            let buffer = await image.getBufferAsync(Jimp.AUTO);
+            console.log(`The length of the buffer from jump is ${buffer.length}`)
+            let stringBuffer = buffer.toString('hex');
+
+            // console.log(`IMge before is ${buffer} and size is ${buffer.length}`)
+            let imgFormat = metadata.common.picture[0].format;
+
+            // let { rows: [album_id] } = await query(`INSERT INTO ALBUMS(Name)
+            //    VALUES($1) ON CONFLICT (Name) DO NOTHING RETURNING id`, [album])
+            // if (!album_id) ({ rows: [album_id] } = await query(`SELECT id FROM Albums Where Name = $1;`, [album]));
+            // console.log(album_id)
+            let {rows:[postBuffer]}=await query(`INSERT INTO SONGS (Name,Length,Year,Hash,imgformat,imgdata)
+            VALUES($1,$2,$3,$4,$5,$6)ON CONFLICT (NAME) DO NOTHING  `,[title, length, year, hashValue, imgFormat, stringBuffer])
+        //    let { rows: [imgBuffer] }= await query(`INSERT INTO SONGS(Name,Length,Year,Hash,imgformat,imgdata)
+        //        VALUES($1,$2,$3,$4,$5,$6)  RETURNING imgdata`,
+        //         [title, length, year, hashValue, imgFormat, stringBuffer])
+            //  console.log(`Img after is ${imgBuffer}`)
         }
         catch (err) {
             console.log(musicFileList[i])
             console.log(err)
         }
-        // query(`INSERT INTO SONGS(Name,Length,Year)
-        //        VALUES($1,$2,$3)`, [title,length,year])
+
         // if(!Circles){
         //     console.log(title);
         // }
@@ -75,13 +91,6 @@ let createBasicTables = () => {
         Name TEXT UNIQUE
     );`)
     query(`CREATE TABLE IF NOT EXISTS ALBUMS(
-        id SERIAL PRIMARY KEY,
-        Name TEXT UNIQUE
-    );`)
-}
-
-let createTableLinks = () => {
-    query(`CREATE TABLE IF NOT EXISTS songVocalists(
         id SERIAL PRIMARY KEY,
         Name TEXT UNIQUE
     );`)
