1461d35686c4090462eb4913fc84290c08ec0739
Poreniyi <poreniyi@gmail.com>
2021-04-13 12:54:28 -0400

    added an sql file to drop tables

---
+++ DB/setQueries/dropAllTables.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS songs,arrangers,albums,vocalists,circles;--base tables
+DROP TABLE IF EXISTS song_vocalists,song_arrangers,circle_albums,circle_arrangers,circle_songs;--relation tables
