cf037c050a664e1ef17bc61648fdddd185dc7fad
Poreniyi <poreniyi@gmail.com>
2021-04-13 13:47:52 -0400

    added  a do nothing on same album name conflict for albums table insert

--- DB/setQueries/insertMetadata.js
+++ DB/setQueries/insertMetadata.js
@@ -4,27 +4,36 @@ let fs = require('fs').promises
 let path = require('path');
 
 let insertData = async () => {
+    let currentAlbumId;
     let musicFileList = await fs.readdir(path.join(__dirname, '../../../musicfiles'));
     for (let i = 0; i < musicFileList.length; i++) {
-        let metadata = await musicParser.parseFile(path.join(__dirname, `../../../musicfiles/${musicFileList[i]}`));
+      try { let metadata = await musicParser.parseFile(path.join(__dirname, `../../../musicfiles/${musicFileList[i]}`));
         let title = metadata.common.title.trim();
         let album = metadata.common.album;
         let Vocalists = metadata.common.comment[0].split('/');//array
         let Arrangers = metadata.common.composer;//array
         let Circles = metadata.common.albumartist;//should be an array but not
-        let year=metadata.common.year;
-        let length=metadata.format.duration;
-        Circles=Circles||''
-        let circleList=[];
-        circleList=Circles.split('/');
-        if(Circles.includes('/')){
-            console.log(title);
+        let year = metadata.common.year;
+        let length = metadata.format.duration;
+        Circles = Circles || ''
+        let circleList = [];
+        console.log(musicFileList[i])
+        circleList = Circles.split('/');
+        // if (Circles.includes('/')) {
+        //     console.log(title);
+        // }
+         {rows:currentAlbumId}=await query(`INSERT INTO ALBUMS(Name)
+               VALUES($1) ON CONFLICT (Name) DO NOTHING RETURNING id`, [album])
+        if(album_id.length!=0) console.log(album_id)}
+        catch(err){
+            console.log(musicFileList[i])
+            console.log(err)
         }
+        // query(`INSERT INTO SONGS(Name,Length,Year)
+        //        VALUES($1,$2,$3)`, [title,length,year])
         // if(!Circles){
         //     console.log(title);
         // }
-        // query(`INSERT INTO SONGS(Name,Length,Year)
-        //        VALUES($1,$2,$3)`, [title,length,year])
         // query(`INSERT INTO ALBUMS(Name)
         //        VALUES($1)`, [album])
         // Vocalists.forEach(element => {
@@ -35,16 +44,16 @@ let insertData = async () => {
         //     query(`INSERT INTO ARRANGERS(Name)
         //     VALUES($1)`, [element])
         // }) 
-        circleList.forEach(element=>{
-            query(`INSERT INTO Circles(Name)
-            VALUES($1)`, [element])
-        }) 
+        // circleList.forEach(element => {
+        //     query(`INSERT INTO Circles(Name)
+        //     VALUES($1)`, [element])
+        // })
     }
 
 
 }
 insertData()
-let createBasicTables=()=>{
+let createBasicTables = () => {
     query(`CREATE TABLE IF NOT EXISTS VOCALISTS(
         id SERIAL PRIMARY KEY,
         Name TEXT UNIQUE
@@ -66,10 +75,10 @@ let createBasicTables=()=>{
     query(`CREATE TABLE IF NOT EXISTS ALBUMS(
         id SERIAL PRIMARY KEY,
         Name TEXT UNIQUE
-    );`)   
+    );`)
 }
 
-let createTableLinks=()=>{
+let createTableLinks = () => {
     query(`CREATE TABLE IF NOT EXISTS songVocalists(
         id SERIAL PRIMARY KEY,
         Name TEXT UNIQUE
