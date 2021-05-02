ab5045afa363895a938053bf5fbd09c5dc7a8bc3
Poreniyi <poreniyi@gmail.com>
2021-05-01 21:23:16 -0400

    added imgformat and img data columns to songs

--- routes/addSongs.js
+++ routes/addSongs.js
@@ -3,6 +3,7 @@ let router = require('express').Router();
 let musicParser = require('music-metadata');
 let fs = require('fs').promises;
 let path = require('path');
+let Jimp = require('jimp');
 
 router.get('/getSongs', async (req, res) => {
     let data = await query(`SELECT * FROM SONGS ORDER BY id ASC`);
@@ -17,4 +18,28 @@ router.get('/getSongs', async (req, res) => {
     res.render('viewSongs', { data, img })
 })
 
+router.get('/viewSong', async (req, res) => {
+    let metadata = await musicParser.parseFile(path.join(__dirname, `../../musicfiles/-Everlasting Happiness-.mp3`));
+    ({ rows: [img] } = await query(`SELECT name imgformat,imgdata FROM SONGS LIMIT 1`));
+    console.log(metadata.common.picture[0])
+    console.log(img)
+    console.log(img.imgdata.length);
+    let data = { format: img.imgformat, base: img.imgdata.toString('base64') }
+    res.render('view1song', { data })
+})
+
+router.get('/test', async (req, res) => {
+    let metadata = await musicParser.parseFile(path.join(__dirname, `../../musicfiles/-Everlasting Happiness-.mp3`));
+    let image = await Jimp.read(metadata.common.picture[0].data)
+    let imgArray = [];
+    for (let i = 5; i <= 100; i+=5) {
+        console.log(i)
+        image.quality(i);
+        let buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+        console.log(buffer.length)
+        buffer=buffer.toString('base64');
+        imgArray.push(buffer)
+    }
+    res.render('test', {imgArray})
+})
 module.exports = router;
